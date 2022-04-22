c0f956b825bed094bcf15372c9e488e3b1889beb
Nikola Micic <34707374+nikolamicic21@users.noreply.github.com>
2022-04-22 09:11:30 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const quotes = [
   'Gde ste mačkići',
   'Sve to ide po inerciji',
   'Vaš posao je deco sedeći',
-  'Buongiorno, come stai signore. Bene, molto bene',
+  'Buongiorno, come stai signore? Bene, molto bene ili najjebene?',
   'Pa ja vam pričam, a vi kako hoćete'
 ];
 
